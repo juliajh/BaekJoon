d32a6f5a708e1701ff52b021532fb7171144c7dd
JH Park <54497150+juliajh@users.noreply.github.com>
2021-11-10 03:47:31 +0900

    Delete BaekJoon/python directory

--- BaekJoon/python/1520.py
+++
@@ -1,9 +0,0 @@
-a, b = map(int, input().split())
-
-numbers = []
-for i in range(a):
-    num = input()
-    num_list = list(map(int, num.split(maxsplit=b)))
-    numbers.append(num_list)
-
-startnum = numbers[0][0]
